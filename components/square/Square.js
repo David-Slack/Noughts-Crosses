36c55cc1e2c669194b0d5e6906b82b7c6a20ef18
Dave Slack <davidkslack@yahoo.co.uk>
2024-09-03 09:55:44 +0100

    #579 Noughts & Crosses: Added basic Next.js and added the Square, board and styles

---
+++ components/square/Square.js
@@ -0,0 +1,20 @@
+"use client"
+
+import { useState } from 'react';
+
+export function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue('X');
+    }
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
+}
